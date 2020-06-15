2d5c13706e080a13df4b94b4fbb286b0ca0a08e0
Louis Bemberg <louis.bemberg@gmail.com>
2020-06-15 17:54:55 +0200

    Finished models with Jonas and added table relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :countries
+  has_many :countries, through: :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
