7b39720d9c0563dd68ac5fc768975cac1c791966
Louis Bemberg <louis.bemberg@gmail.com>
2020-06-17 20:32:35 +0200

    implemented geocoding setups with migration of lg lt, address. also updates seeds to include addresses. next step is setting up mapbox API

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,7 @@
 class Country < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   # @country.user, shows owner of the country
   has_many :users, through: :bookings
   has_one_attached :photo
