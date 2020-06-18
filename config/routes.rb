8a581c2a31833a263cfd30c26145bffe4e74629d
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-18 11:49:01 +0200

    controller and routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/crash', to: 'pages#crash'
-  resources :countries, only: [:index, :new, :create, :show]
+  resources :countries, only: [:index, :new, :create, :show] do
+    resources :bookings, only: [:index, :new, :create, :show, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
